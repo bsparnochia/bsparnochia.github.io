7e92371389bb4b28b8cefb4bb1318930360d60c6
bsparnochia <brian.sparnochia@gmail.com>
2023-02-27 19:28:35 -0300

    ya se generan perfiles aleatorios dentro del curriculum, cv datos completos

--- javascript/main.js
+++ javascript/main.js
@@ -1,20 +1,25 @@
+/* generarPerfil() aprovecha las funcionalidades que provee la API de randomuser
+ y captura los usuarios aleatorios para generar el curriculum con sus datos*/
 const generarPerfil = async() =>{
   try{
-    const url="https://randomuser.me/api/"
+    const url="https://randomuser.me/api/?inc=name,dob,location,nat,cell,email,picture"
     const consulta = await fetch(url);
     const {results} = await consulta.json();
     const datos=results[0];
-    //comprobamos que los datos del JSON llegaron
-    console.log(datos);
 
-    //Asignación de datos obtenidos de randomuser a  index.html
-    document.getElementById("info").innerHTML = "Soy "+ datos.name.first+" "+datos.name.last +" y mi numero es: "+datos.cell;
+    /* Asignación de datos obtenidos de randomuser.me 
+    Edito nombre y apellido, edad, ubicación, teléfono y email según los datos recibido*/
 
+    document.getElementById("imagen-cv").src=datos.picture.large;
+    document.getElementById("nombre-apellido").innerHTML=datos.name.first+" "+datos.name.last;
+    document.getElementById("edad").innerHTML=datos.dob.age +" años";
+    document.getElementById("ubicacion").innerHTML=datos.location.state+", "+datos.location.country;
+    document.getElementById("telefono").innerHTML=datos.cell;
+    document.getElementById("email").innerHTML=datos.email;
   }catch(error){
     console.log(error);
   }
 }
 
+/* Se ejecuta generarPerfil() en cuanto la página a cargado completamente"*/
 document.addEventListener('DOMContentLoaded',generarPerfil());
-
-//BIIIEEEEEEEN VAMO LO PIBE
